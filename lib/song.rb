8faa63ea3d8787e1ddf66d76723b688fede90cde
Ishani <ishaniamin@gmail.com>
2018-06-19 20:22:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,4 +29,12 @@ class Song
    @@artists.uniq
  end
 
+ def self.genre_count
+    genre_hash = Hash.new(0)
+    @@genres.each do |g|
+      genre_hash[g] += 1
+    end
+    return genre_hash
+  end
+
 end
