016beb344e1e393d8323a8c8de783a30df5b062a
Ishani <ishaniamin@gmail.com>
2018-06-19 20:24:51 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,48 @@
+class Song
 
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+
+    @@artists << artist
+    @@genres << genre
+
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+   @@genres.uniq
+  end
+
+ def self.artists
+   @@artists.uniq
+ end
+
+ def self.genre_count
+    genre_hash = Hash.new(0)
+    @@genres.each do |newGen|
+      genre_hash[newGen] += 1
+    end
+    genre_hash
+  end
+
+  def self.artist_count
+     artist_hash = Hash.new(0)
+     @@artists.each do |newArt|
+       artist_hash[newArt] += 1
+     end
+     artist_hash
+   end
+
+end
